b5d6a9877852b8e831e1f4de61790d2292a549fd
Austin Conrad <austinconrad@Austins-MacBook-Pro-3.local>
2022-03-28 10:42:32 -0400

    drop downs updated

---
+++ test-natsjs/src/routes/a/gps.svelte
@@ -0,0 +1,28 @@
+<style>
+	h4{
+		font-size: 400%;
+		text-align: center;
+	}
+	body{
+		border-style: solid;
+		border-width: 1%;
+		background-color: aqua;
+
+	}
+    a{
+		font-family: 'Times New Roman', Times, serif;
+		background-color: black;
+		color: white;
+		border-color: black;
+		border-style: solid;
+		border-width: .005%;
+		font-size: 150%;
+	}
+</style>
+
+<title>GPS Page</title>
+<body>
+    <a href="/a/test">Go to debugger page</a>
+    <h4>GPS Tracking</h4>
+    <a href="/">Go to Main Page</a>
+</body>
