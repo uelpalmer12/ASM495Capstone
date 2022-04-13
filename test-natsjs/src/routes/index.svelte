1bf4d27f2f69beefcebcb4a350265f2bf39347c4
AustinConrad2000 <conrad52@purdue.edu>
2022-04-13 19:07:55 -0400

    all pages working

--- test-natsjs/src/routes/index.svelte
+++ test-natsjs/src/routes/index.svelte
@@ -4,26 +4,101 @@
 
   	import AutoComplete from "simple-svelte-autocomplete"
 
+	import { connect, Empty, JSONCodec } from 'nats.ws';
+
+	let nc;
+	  async function doWork(address) {
+		if(!address) {
+			return;
+		}
+
+		if (nc) {
+			nc.close();
+		}
+
+
+		// Required step 1
+		// NOTE: This should be moved so it only happens once ... I can help later when your ready.
+		
+		nc = await connect({ servers: address });
+		
+
+		// create a codec
+		const jc = JSONCodec();
+
+		// Required step 2
+		
+		await nc.request("j1939.keys", Empty, { timeout: 1000 })
+		.then((m) => {
+			let subjects = jc.decode(m.data);
+
+			ObjectListOne = [];
+			subjects.forEach(subject => {
+				console.log(subject)
+				ObjectListOne.push({
+					name1: translate[subject] || subject,
+					obj1:  subject
+				})
+			});
+		// 	console.log(`got response: ${subjects}`);
+		 })  
+		.catch((err) => {
+			console.log(`problem with request: ${err.message}`);
+		});
+
+		await nc.close();
+	}
+
+	$: doWork(ip);
+
+	const translate = {
+		"j1939.filter.EngFuelRate": "Engine Fuel Rate",
+		"j1939.filter.EngSpeed": "Engine Speed",
+		"j1939.filter.EngPercentLoadAtCurrentSpeed": "Engine Percent Load at Current Speed",
+		"j1939.filter.ActualEngPercentTorque": "Actual Engine Percent Torque",
+		"j1939.filter.EngTorqueMode": "Engine Torque Mode",
+		"j1939.filter.MalfunctionIndicatorLampStatus": "Malfuction Indicator Lamp Status",
+		"j1939.filter.AmberWarningLampStatus": "Amber Warning Lamp Status",
+		"j1939.filter.ProtectLampStatus": "Protect Lamp Status",
+		"j1939.filter.RedStopLampState": "Red Stop Lamp Status",
+		"j1939.filter.EnginesDesiredOperatingSpeed": "Engines Desired Operating Speed",
+		"j1939.filter.WheelBasedVehicleSpeed": "Wheel Based Vehicle Speed",
+		"j1939.filter.Latitude": "Latitued",
+		"j1939.filter.Longitude": "Longitude",
+		"j1939.filter.EngIntakeManifold1Press": "Engine Intake Manifold 1 Press",
+		"j1939.filter.EngIntakeManifold1Temp": "Engine Intake Manifold 1 Temp",
+		"j1939.filter.TransCurrentGear": "Transmission Current Gear",
+		"j1939.filter.NavigationBasedVehicleSpeed": "Navigation Based Vehicle Speed",
+		"j1939.filter.FuelLevel1": "Fuel Level 1",
+		"j1939.filter.EngFuelTemp1": "Engine Fuel Temperature",
+		"j1939.filter.EngCoolantTemp": "Engine Coolant Temperature",
+		"j1939.filter.KeyswitchBatteryPotential": "Key Switch Batter Potential",
+		"j1939.filter.TotalPowerTakeoffHours": "Total Power Take Off Hours",
+		"j1939.filter.BatteryPotential_PowerInput1": "Battery Potential Power Input 1",
+		"j1939.filter.EngTotalFuelUsed": "Engine Total Fuel Used"
+	}
 
   	const IPList = [
   	{ id: 1, name: "Combine", ip: '123.24.24.23' },
   	{ id: 2, name: "Tractor one", ip: 'ws://172.16.254.5:443' },
   	]
-	const ObjectListOne = [
+	let ObjectListOne = [
   	{ id: 1.1, name1: "Engine Fuel Rate", obj1: 'j1939.filter.EngFuelRate' },
-  	{ id: 2.1, name1: "Engine Speed", obj1: 'j1939.filter.EngSpeed' },
+	{ id: 2.1, name1: "Engine Speed", obj1: 'j1939.filter.EngSpeed' },
 	{ id: 3.1, name1: "Nav Based Vehicle Speed", obj1: 'j1939.filter.NavigationBasedVehicleSpeed' },
-	{ id: 3.1, name1: "Joystick", obj1: 'j1939.filter.Joystick2Button9PressedStatus' },
-	{ id: 3.1, name1: "Wheel Based Vehicle Speed", obj1: 'j1939.filter.WheelBasedVehicleSpeed' },
-	]
+	{ id: 3.1, name1: "Engine Total Fuel Used", obj1: 'j1939.filter.EngTotalFuelUsed' },
+	{ id: 3.1, name1: "Engine Percent Load at Current Speed", obj1: 'j1939.filter.EngPercentLoadAtCurrentSpeed' },
+	{ id: 3.1, name1: "Actual Engine Percent Torqued", obj1: 'j1939.filter.ActualEngPercentTorque' }
 
+	]
+ 
   	let selectedIP = IPList[1];
 	let selectedOBJ1 = ObjectListOne[0];
 	let selectedOBJ2 = ObjectListOne[1];
 	let selectedOBJ3 = ObjectListOne[2];
 	let selectedOBJ4 = ObjectListOne[3];
 	let selectedOBJ5 = ObjectListOne[4];
-	let selectedOBJ6 = {};
+	let selectedOBJ6 = ObjectListOne[5];
 
   	let ip;
 	let obj1;
@@ -56,6 +131,8 @@
 		name5 = selectedOBJ5.name1
 		name6 = selectedOBJ6.name1
 	}
+
+
 
 </script>
 
